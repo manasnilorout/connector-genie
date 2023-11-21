47de87540cbd62ed1eaa7ff41be332b9f2bfbda1
Manas Ranjan <manas.nilorout@uipath.com>
2023-11-21 11:36:39 +0530

    Added readme and some modifications

--- src/components/ApiResponse.js
+++ src/components/ApiResponse.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { tomorrowNight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import './ApiResponse.css';
 
 function ApiResponse({ response }) {
     return (
-        <div className="markdown-content">
-            <ReactMarkdown components={{
-                code({ node, inline, className, children, ...props }) {
-                    const match = /language-(\w+)/.exec(className || '')
-                    return !inline && match
-                        ? <SyntaxHighlighter style={tomorrowNight} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
-                        : <code className={className} {...props}>{children}</code>
-                }
-            }}>
-                {response}
-            </ReactMarkdown>
-        </div>
+      <div className="markdown-content">
+        <ReactMarkdown components={{
+          code({node, inline, className, children, ...props}) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match
+              ? <SyntaxHighlighter style={dark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+              : <code className={className} {...props}>{children}</code>
+          }
+        }}>
+          {response}
+        </ReactMarkdown>
+      </div>
     );
-}
+  }
 
 export default ApiResponse;
