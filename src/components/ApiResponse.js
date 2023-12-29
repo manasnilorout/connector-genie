dee6e157909b73351c74769055f9ddbd1c040268
Abhishek Tiwari <abhishek.tiwari@uipath.com>
2023-12-29 14:58:22 +0530

    feat: Added copy to clipboard functionality

--- src/components/ApiResponse.js
+++ src/components/ApiResponse.js
@@ -2,24 +2,25 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
+import CopyToClipboard from './CopyToClipboard';
 import './ApiResponse.css';
 
 function ApiResponse({ response }) {
-    return (
-      <div className="markdown-content">
-        <ReactMarkdown components={{
-          code({node, inline, className, children, ...props}) {
-            const match = /language-(\w+)/.exec(className || '')
-            return !inline && match
-              ? <SyntaxHighlighter style={dark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
-              : <code className={className} {...props}>{children}</code>
-          }
-        }}>
-          {response}
-        </ReactMarkdown>
-      </div>
-    );
-  }
+  return (
+    <div className="markdown-content">
+      {response && <CopyToClipboard text={response} />}
+      <ReactMarkdown components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '')
+          return !inline && match
+            ? <SyntaxHighlighter style={dark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+            : <code className={className} {...props}>{children}</code>
+        }
+      }}>
+        {response}
+      </ReactMarkdown>
+    </div>
+  );
+}
 
 export default ApiResponse;
